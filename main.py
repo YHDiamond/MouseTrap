fdd6f935d7f88571e3b6863aa2e2f98fe88d3f36
YHDiamond <47502993+yehudahrrs@users.noreply.github.com>
2022-03-21 13:38:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import requests
 import json
 import os
 from twilio.rest import Client
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21, GPIO.IN)
+
 
 number = os.getenv('TWILIO_NUMBER')
 
@@ -11,5 +17,22 @@ client = Client()
 def text(recipient, message):
     client.messages.create(to=recipient, from_=number, body=message)
 
-text('+12014495049', 'Hello!')
+caught = False
 
+while True:
+
+
+    if (GPIO.input(21)):
+        if not caught:
+            time.sleep(1)
+            if (GPIO.input(21)):
+                print("Motion Detected!")
+                text('+12014495049', 'Mouse exist!')
+                caught = True
+    else: 
+        if (caught):
+            time.sleep(300)
+            if (not GPIO.input(21)):
+                caught = False
+        
+    
